2c68d0333dcbf4916c2d85bad350b782fc1d3cdd
beautiful-cookie <maxim.khadeev@gmail.com>
2023-03-11 23:55:12 +0300

    some stylistic Recipe component updates

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -12,6 +12,7 @@ const Recipe = (props) => {
   const [recipe_name, setRecipe_name] = useState(""); 
   const [recipe_img, setRecipe_img] = useState(""); 
   const [recipe_calories, setRecipe_calories] = useState(""); 
+  const [recipe_totalWeight, setRecipe_totalWeight] = useState(""); 
 
   useEffect(() => {
     fetch(`${apiUrl}${recipe_id}?type=public&app_id=${appId}&app_key=${appKey}`)
@@ -21,15 +22,32 @@ const Recipe = (props) => {
       let recipe = responceJSON.recipe 
       setRecipe_name(recipe.label) 
       setRecipe_img(recipe.image) 
-      setRecipe_calories(recipe.calories) 
+      setRecipe_calories(recipe.calories.toFixed(2)) 
+      setRecipe_totalWeight(recipe.totalWeight.toFixed(2)) 
     })
   }, [recipe_id]) 
 
   return (
     <div className={styles.recipeWrapper}> 
-      <img src={recipe_img} alt="" /> 
-      {recipe_name} 
-      {recipe_calories} 
+      <div className={styles.imageWrapper}>
+        <img src={recipe_img} alt="" /> 
+      </div> 
+
+      <h2>{recipe_name}</h2>
+
+      <div className={styles.detailsWrapper}>
+        <div className={styles.detailsContainer}>
+          <p>
+            <span className={styles.detailsText}>Calories:</span> {recipe_calories}
+          </p>
+          <p>
+            <span className={styles.detailsText}>Weight:</span> {recipe_totalWeight}
+          </p>
+          <p>
+            <span className={styles.detailsText}>Calories:</span> {recipe_calories}
+          </p>
+        </div>
+      </div> 
     </div>
   )
 } 
