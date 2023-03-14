af48457bcf57e2f23f58f987e4c57e5312c786b3
beautiful-cookie <maxim.khadeev@gmail.com>
2023-03-14 23:09:33 +0300

    search tab activate

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import styles from './Header.module.css' 
 import SearchIcon from '@mui/icons-material/Search';
-import StarBorderIcon from '@mui/icons-material/StarBorder';
+import StarBorderIcon from '@mui/icons-material/StarBorder'; 
+import { Link } from 'react-router-dom'; 
 // Пусть Навигация будет разбира по якорям страницы 
 // Придумай еще какие-то разделы для якорей 
 const Header = () => {
@@ -10,7 +11,9 @@ const Header = () => {
         <span className={styles.empty}></span>
         <span className={styles.logo}>Recipes</span>
         <div className={styles.tabs}>
-          <span className={styles.searchTab}> <SearchIcon sx={{ fontSize: 20 }} /> Search</span>
+          <Link to='/' className={styles.searchTab}>
+            <span> <SearchIcon sx={{ fontSize: 20 }} /> Search</span>          
+          </Link>
           <span className={styles.favorites}> <StarBorderIcon sx={{ fontSize: 20 }} /> Favorites</span>
         </div>
       </div>
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,5 +1,6 @@
 import styles from './Search.module.css' 
 import React, { useState } from 'react'; 
+import { Link } from 'react-router-dom'; 
 
 const Search = (props) => {
     const [search, setSearch] = useState(""); 
@@ -22,7 +23,9 @@ const Search = (props) => {
     return (
         <div className={styles.SearchWrapper}> 
             <input placeholder='Поиск...' type="text" value={search} onChange={setSearchValue} onKeyPress={handleKeyPress} className={styles.Search} /> 
-            <input type='submit' value='Search' onClick={searchFunction} className={styles.SearchButton} /> 
+            <Link to='/#' className={styles.SearchButton}>
+                <div onClick={searchFunction}>Search</div>
+            </Link>
         </div>
     )
 } 
